129188393d7618f05455fb6eccc6359a2c5fcd2a
ByakuranN7 <51470781+ByakuranN7@users.noreply.github.com>
2026-06-13 08:41:45 -0300

    feat: adiciona classificação de IMC
    - Adiciona classificação baseada nas faixas de IMC
    - Cria método classificarIMC()
    - Separa cálculo e classificação em métodos distintos
    - Melhora a organização e legibilidade do código

--- 01 - Calculadora de IMC/CalculadoraIMC.java
+++ 01 - Calculadora de IMC/CalculadoraIMC.java
@@ -1,18 +1,39 @@
 //não esquecer de adicionar importações ao inserir os elementos
 
-public class CalculadoraIMC{
+public class CalculadoraIMC {
 
 
-    public double calculoIMC(double peso, double altura){
-        return peso/(altura*altura);
+    public static double calculoIMC(double peso, double altura) {
+        return peso / (altura * altura);
     }
 
+    public static void classificarIMC(double imc){
+        if (imc < 17) {
+            System.out.println("\nMuito abaixo do peso.");
+        } else if (imc < 18.5) {
+            System.out.println("\nAbaixo do peso.");
+        } else if (imc < 25) {
+            System.out.println("\nPeso normal.");
+        } else if (imc < 30) {
+            System.out.println("\nAcima do peso.");
+        } else if (imc < 35) {
+            System.out.println("\nObesidade I.");
+        } else if (imc < 40) {
+            System.out.println("\nObesidade II (severa)");
+        } else {
+            System.out.println("\nObesidade III (mórbida)");
+        }
+    }
 
-
-    public static void main(String[] args){
-        double peso = 88.3; //em kg
+    public static void main(String[] args) {
+        double peso = 75.3; //em kg
         double altura = 1.80; //em m
+        double imc;
 
-        System.out.println("O IMC de uma pessoa com peso de " + peso + "kg e uma altura de " + altura "m é: " + calculoIMC(peso,altura));
+        imc = calculoIMC(peso, altura);
+
+        System.out.println("O IMC de uma pessoa com peso de " + peso + "kg e uma altura de " + altura + "m é: " + imc);
+        classificarIMC(imc);
     }
+
 }
