5a5fe0efdbd959ab4fd4b27ff4deb4728f73bc76
ByakuranN7 <51470781+ByakuranN7@users.noreply.github.com>
2026-06-21 04:51:58 -0300

    feat: adicionar botão de voltar
    - Botão "voltar" adicionado, com sua implementação de retornar para a URL anterior.
    
    - Adicionado suporte ao botão voltar do mouse.

--- 05 - Mini Navegador Web/MiniWebBrowser.java
+++ 05 - Mini Navegador Web/MiniWebBrowser.java
@@ -4,30 +4,48 @@ import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
 import javafx.scene.control.Button;
 import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseButton;
 
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
+import javafx.scene.web.WebHistory;
 
 public class MiniWebBrowser extends Application {
 
     @Override
     public void start(Stage palco) {
         TextField campoUrl = new TextField();
+        Button botaoVoltar = new Button("←");
         Button botaoAtualizar = new Button("Atualizar");
         WebView navegador = new WebView();
         WebEngine motor = navegador.getEngine();
+        WebHistory webHistory = motor.getHistory();
         motor.load("https://www.google.com/");
 
         // Carregar uma página da web quando o usuário pressiona Enter
         campoUrl.setOnAction(evento -> motor.load(formataUrl(campoUrl.getText())));
 
         botaoAtualizar.setOnAction(evento -> motor.reload());
+        botaoVoltar.setOnAction(evento -> {
+            if (webHistory.getCurrentIndex() > 0) {
+                webHistory.go(-1);
+            }
+        });
 
         VBox layoutPrincipal = new VBox();
-        layoutPrincipal.getChildren().addAll(botaoAtualizar, campoUrl, navegador);
+        layoutPrincipal.getChildren().addAll(botaoVoltar, botaoAtualizar, campoUrl, navegador);
         Scene cena = new Scene(layoutPrincipal);
+
+        cena.setOnMousePressed(evento -> {
+            if (evento.getButton() == MouseButton.BACK) {
+                if (webHistory.getCurrentIndex() > 0) {
+                    webHistory.go(-1);
+                }
+            }
+        });
 
         cena.setOnKeyPressed(evento -> {
             if (evento.getCode() == KeyCode.F5) {
